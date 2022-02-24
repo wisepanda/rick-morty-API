0888b9bb0c9b541324ccc0ab1841dfacbc8060a5
wisepanda <93228256+wisepanda@users.noreply.github.com>
2022-02-24 17:23:44 +0000

    Updates

--- src/Components/Button/button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Button() {
-    return (
-        <div className='button-container'>
-            <button className='button'>"Wubbalubbadubdub!"</button>
-        </div>
-    )
-}
